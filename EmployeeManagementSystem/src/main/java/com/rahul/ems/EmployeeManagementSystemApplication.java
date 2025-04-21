812367138bdde343b0e33500499156e4d2510abd
Rahul <150949426+RahulPamukuntla@users.noreply.github.com>
2025-04-22 04:50:04 +0530

    Update EmployeeManagementSystemApplication.java
    added @Override method

--- EmployeeManagementSystem/src/main/java/com/rahul/ems/EmployeeManagementSystemApplication.java
+++ EmployeeManagementSystem/src/main/java/com/rahul/ems/EmployeeManagementSystemApplication.java
@@ -2,10 +2,17 @@ package com.rahul.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBoot
 
 @SpringBootApplication
-public class EmployeeManagementSystemApplication {
+public class EmployeeManagementSystemApplication extends SpringBootServletInitializer {
 
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
